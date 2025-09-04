dae0b20c471b4cf6bccdfdc2087285080c651e63
Halil Ibrahim Demirci <hademirc@k1m32s11.42istanbul.com.tr>
2025-09-04 16:22:03 +0300

    Changed structs to preference.

--- includes/structures.h
+++ includes/structures.h
@@ -1,64 +1,55 @@
 #ifndef STRUCTURES_H
 # define STRUCTURES_H
 
-/* Forward declarations */
-typedef struct s_texture	t_texture;
-typedef struct s_player		t_player;
-typedef struct s_map		t_map;
-typedef struct s_config		t_config;
-typedef struct s_graphics	t_graphics;
-typedef struct s_raycast	t_raycast;
-typedef struct s_game		t_game;
-
 /* Texture structure */
-struct s_texture
+typedef struct s_texture
 {
-	void	*img;
-	char	*data;
-	int		width;
-	int		height;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-};
+	void		*img;
+	char		*data;
+	int			width;
+	int			height;
+	int			bits_per_pixel;
+	int			line_length;
+	int			endian;
+}				t_texture;
 
 /* Player structure */
-struct s_player
+typedef struct s_player
 {
-	double	pos_x;
-	double	pos_y;
-	double	dir_x;
-	double	dir_y;
-	double	plane_x;
-	double	plane_y;
-	double	move_speed;
-	double	rot_speed;
-};
+	double		pos_x;
+	double		pos_y;
+	double		dir_x;
+	double		dir_y;
+	double		plane_x;
+	double		plane_y;
+	double		move_speed;
+	double		rot_speed;
+}				t_player;
 
 /* Map structure */
-struct s_map
+typedef struct s_map
 {
-	char	**grid;
-	int		width;
-	int		height;
-	int		player_x;
-	int		player_y;
-	char	player_dir;
-};
+	char		**grid;
+	int			width;
+	int			height;
+	int			player_x;
+	int			player_y;
+	char		player_dir;
+}				t_map;
 
 /* Configuration structure */
-struct s_config
+typedef struct s_config
 {
-	char	*north_texture;
-	char	*south_texture;
-	char	*west_texture;
-	char	*east_texture;
-	int		floor_color;
-	int		ceiling_color;
-};
+	char		*north_texture;
+	char		*south_texture;
+	char		*west_texture;
+	char		*east_texture;
+	int			floor_color;
+	int			ceiling_color;
+}				t_config;
 
 /* Graphics structure */
-struct s_graphics
+typedef struct s_graphics
 {
 	void		*mlx;
 	void		*window;
@@ -68,39 +59,39 @@ struct s_graphics
 	int			line_length;
 	int			endian;
 	t_texture	textures[4];
-};
+}				t_graphics;
 
 /* Raycasting structure */
-struct s_raycast
+typedef struct s_raycast
 {
-	double	camera_x;
-	double	ray_dir_x;
-	double	ray_dir_y;
-	int		map_x;
-	int		map_y;
-	double	delta_dist_x;
-	double	delta_dist_y;
-	double	perp_wall_dist;
-	int		step_x;
-	int		step_y;
-	int		hit;
-	int		side;
-	int		line_height;
-	int		draw_start;
-	int		draw_end;
-	int		tex_num;
-	double	wall_x;
-	int		tex_x;
-};
+	double		camera_x;
+	double		ray_dir_x;
+	double		ray_dir_y;
+	int			map_x;
+	int			map_y;
+	double		delta_dist_x;
+	double		delta_dist_y;
+	double		perp_wall_dist;
+	int			step_x;
+	int			step_y;
+	int			hit;
+	int			side;
+	int			line_height;
+	int			draw_start;
+	int			draw_end;
+	int			tex_num;
+	double		wall_x;
+	int			tex_x;
+}				t_raycast;
 
 /* Main game structure */
-struct s_game
+typedef struct s_game
 {
 	t_player	player;
 	t_map		map;
 	t_graphics	graphics;
 	t_config	config;
 	t_raycast	raycast;
-};
+}				t_game;
 
 #endif
