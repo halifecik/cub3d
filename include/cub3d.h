64dac866abc1dd5494269ea73439a1bf17d9f324
Muhammed Sabit Genan <mugenan@k1m04s06.42istanbul.com.tr>
2025-09-04 13:45:51 +0300

    miniblix library has been fixed

---
+++ include/cub3d.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mugenan <mugenan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/04 13:37:23 by mugenan           #+#    #+#             */
+/*   Updated: 2025/09/04 13:41:07 by mugenan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef CUB3D_H
+# define CUB3D_H
+
+#include <math.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <limits.h>
+
+#endif
