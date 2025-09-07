c093b29161258739e10ccaef7b0d86dc79a55b09
Halil Demirci <146574700+halifecik@users.noreply.github.com>
2025-09-07 14:51:23 +0000

    refactor: improve readability with local ray pointer

--- src/raycasting/raycast_core.c
+++ src/raycasting/raycast_core.c
@@ -2,20 +2,23 @@
 
 static void	init_ray(t_data *data, int x)
 {
-	data->raycast.camera_x = 2 * x / (double)SCREEN_WIDTH - 1;
-	data->raycast.ray_dir_x = data->player.dir_x + data->player.plane_x * data->raycast.camera_x;
-	data->raycast.ray_dir_y = data->player.dir_y + data->player.plane_y * data->raycast.camera_x;
-	data->raycast.map_x = (int)data->player.pos_x;
-	data->raycast.map_y = (int)data->player.pos_y;
-	if (data->raycast.ray_dir_x == 0)
-		data->raycast.delta_dist_x = 1e30;
+	t_raycast	*ray;
+
+	ray = &data->raycast;
+	ray->camera_x = 2 * x / (double)SCREEN_WIDTH - 1;
+	ray->ray_dir_x = data->player.dir_x + data->player.plane_x * ray->camera_x;
+	ray->ray_dir_y = data->player.dir_y + data->player.plane_y * ray->camera_x;
+	ray->map_x = (int)data->player.pos_x;
+	ray->map_y = (int)data->player.pos_y;
+	if (ray->ray_dir_x == 0)
+		ray->delta_dist_x = 1e30;
 	else
-		data->raycast.delta_dist_x = fabs(1 / data->raycast.ray_dir_x);
-	if (data->raycast.ray_dir_y == 0)
-		data->raycast.delta_dist_y = 1e30;
+		ray->delta_dist_x = fabs(1 / ray->ray_dir_x);
+	if (ray->ray_dir_y == 0)
+		ray->delta_dist_y = 1e30;
 	else
-		data->raycast.delta_dist_y = fabs(1 / data->raycast.ray_dir_y);
-	data->raycast.hit = 0;
+		ray->delta_dist_y = fabs(1 / ray->ray_dir_y);
+	ray->hit = 0;
 }
 
 static void	calculate_step_and_sidedist(t_data *data)
