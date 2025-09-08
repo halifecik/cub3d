b8fe377b4f25bbba526617d8030c12839f51c59a
halifecik <khalifecik@gmail.com>
2025-09-08 18:33:55 +0300

    feat: add current initialization prototypes

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -14,6 +14,15 @@
 // game
 
 // init
+void	ft_player_east(t_data *data);
+void	ft_player_west(t_data *data);
+void	ft_player_north(t_data *data);
+void	ft_player_south(t_data *data);
+void	ft_initialize_player(t_player *player);
+void	ft_initialize_player_location(t_data *data, t_map *map);
+void	ft_initialize_ray(t_data *data, int x);
+
+int	ft_initialize_game(t_data *data);
 
 // parse
 
