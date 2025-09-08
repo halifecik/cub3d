1c92189a7928a8dee9b1e55bc95dbc52e7360e41
halifecik <khalifecik@gmail.com>
2025-09-08 17:43:37 +0300

    refactor: clean header files

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -11,30 +11,16 @@
 # include "gnl/get_next_line.h"
 # include "libft/libft.h"
 
-/* init/init_structs */
-int     init_data(t_data *data);
+// game
 
-/* init/init_player */
-void	init_player_location(t_data *data, t_map *map);
+// init
 
-/* parser/parse_identifier */
-int     is_identifier(char *line);
-void    parse_identifier(char *line, t_config *config);
+// parse
 
-/* parser/parse_cube */
-int     parse_map(t_data *data, char *filename);
+// raycast
 
-/* parser/flood_fill */
-void	reset_flood_fill(t_map *map);
-void	flood_fill(t_map *map, int y, int x, int *valid);
+// utils
 
-/* parser/validate_map */
-int	validate_map(t_map *map);
 
-/* utils/parser_tools */
-int	is_whitespace(char c);
-
-/* main.c */
-int     print_error(char *msg);
 
 #endif
--- includes/graphics.h
+++ includes/graphics.h
@@ -3,25 +3,5 @@
 
 # include "structs.h"
 
-/* graphics/graphics_init.c */
-int		load_textures(t_data *data);
-
-/* raycasting/raycast_core.c */
-void	cast_ray(t_data *data, int x);
-
-/* raycasting/texture_calc.c */
-void	calculate_texture_coordinates(t_data *data);
-int		get_texture_color(t_data *data, int tex_y);
-
-/* raycasting/render.c */
-void	render_frame(t_data *data);
-
-/* game/input_handler.c */
-int		key_press(int keycode, t_data *data);
-
-/* game/game_loop.c */
-int		game_loop(t_data *data);
-int		start_game(t_data *data);
-int		exit_game(t_data *data);
 
 #endif
