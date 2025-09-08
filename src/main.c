24aaaaa38ed86b031deedcdb40052a8d5c6c5c56
halifecik <khalifecik@gmail.com>
2025-09-08 18:45:40 +0300

    style: change name to ft_ and place static function above rest

--- src/main.c
+++ src/main.c
@@ -1,17 +1,6 @@
 #include "cub3d.h"
 
-int	print_error(char *msg)
-{
-	int i = 0;
-
-	write(1, "Error\n", 6);
-	while (msg[i])
-		write(1, &msg[i++], 1);
-	write(1, "\n", 1);
-	return (EXIT_FAILURE);
-}
-
-static int	has_cub_extension(char *filename)
+static int	ft_is_cub_extension(char *filename)
 {
 	size_t	len;
 
@@ -25,24 +14,35 @@ static int	has_cub_extension(char *filename)
 	return (1);
 }
 
+int	ft_print_error(char *msg)
+{
+	int i = 0;
+
+	write(1, "Error\n", 6);
+	while (msg[i])
+		write(1, &msg[i++], 1);
+	write(1, "\n", 1);
+	return (EXIT_FAILURE);
+}
+
 int	main(int argc, char **argv)
 {
 	t_data	*data;
 
 	if (argc != 2)
-		return (print_error("argc is not enough!"));
-	if (has_cub_extension(argv[1]))
-		return (print_error("File must have .cub extension"));
+		return (ft_print_error("argc is not enough!"));
+	if (ft_is_cub_extension(argv[1]))
+		return (ft_print_error("File must have .cub extension"));
 	data = malloc(sizeof(t_data));
 	if (!data)
-		return (print_error("Failed to allocate t_data"));
+		return (ft_print_error("Failed to allocate t_data"));
 	data->map_file = argv[1];
-	if (init_data(data))
-		return (print_error("Failed to initialize data"));
-	if (parse_map(data, data->map_file))
-		return (print_error("Failed to parse .cub file"));
-	if (validate_map(&data->map))
-		return (print_error("Invalid map"));
-	init_player_location(&data->player, &data->map);
+	if (ft_initialize_game(data))
+		return (ft_print_error("Failed to initialize data"));
+	if (ft_parse_map(data, data->map_file))
+		return (ft_print_error("Failed to parse .cub file"));
+	if (ft_validate_map(&data->map))
+		return (ft_print_error("Invalid map"));
+	ft_initialize_player_location(&data->player, &data->map);
 	return (EXIT_SUCCESS);
 }
