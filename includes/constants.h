2053f0d6b5eb9d4fbaf08b8b7142886150dbd8f1
halifecik <khalifecik@gmail.com>
2025-09-20 12:46:49 +0300

    feat: add color to constants

--- includes/constants.h
+++ includes/constants.h
@@ -32,7 +32,7 @@
 # define KEY_RIGHT 124
 # define KEY_ESC 53
 
-/* Colors */
+/* Pixel Colors */
 # define RED_PIXEL 0xFF0000
 # define GREEN_PIXEL 0x00FF00
 # define BLUE_PIXEL 0x0000FF
@@ -49,5 +49,16 @@
 
 /* Mathematical constants */
 # define PI 3.14159265359
+
+/* Text Colors */
+#define BLACK       "\033[0;30m"
+#define RED         "\033[0;31m"
+#define GREEN       "\033[0;32m"
+#define YELLOW      "\033[0;33m"
+#define BLUE        "\033[0;34m"
+#define MAGENTA     "\033[0;35m"
+#define CYAN        "\033[0;36m"
+#define WHITE       "\033[0;37m"
+#define RESET       "\033[0m"
 
 #endif
