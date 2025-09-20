2a3986f7228b39589de8520264b7bd7a7fb76d12
halifecik <khalifecik@gmail.com>
2025-09-20 19:56:18 +0300

    fix: change direction vectors

--- src/init/direction.c
+++ src/init/direction.c
@@ -6,7 +6,7 @@
 /*   By: hademirc <hademirc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/20 18:31:24 by hademirc          #+#    #+#             */
-/*   Updated: 2025/09/20 18:31:25 by hademirc         ###   ########.fr       */
+/*   Updated: 2025/09/20 19:53:42 by hademirc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,32 @@
 
 void	ft_player_north(t_data *data)
 {
-	data->player.dir_x = -1.0;
+	data->player.dir_x = 0.0;
+	data->player.dir_y = -1.0;
+	data->player.plane_x = 0.66;
+	data->player.plane_y = 0.0;
+}
+
+void	ft_player_south(t_data *data)
+{
+	data->player.dir_x = 0.0;
+	data->player.dir_y = 1.0;
+	data->player.plane_x = -0.66;
+	data->player.plane_y = 0.0;
+}
+
+void	ft_player_east(t_data *data)
+{
+	data->player.dir_x = 1.0;
 	data->player.dir_y = 0.0;
 	data->player.plane_x = 0.0;
 	data->player.plane_y = 0.66;
 }
 
-void	ft_player_south(t_data *data)
+void	ft_player_west(t_data *data)
 {
-	data->player.dir_x = 1.0;
+	data->player.dir_x = -1.0;
 	data->player.dir_y = 0.0;
 	data->player.plane_x = 0.0;
 	data->player.plane_y = -0.66;
-}
-
-void	ft_player_east(t_data *data)
-{
-	data->player.dir_x = 0.0;
-	data->player.dir_y = 1.0;
-	data->player.plane_x = 0.66;
-	data->player.plane_y = 0.0;
-}
-
-void	ft_player_west(t_data *data)
-{
-	data->player.dir_x = 0.0;
-	data->player.dir_y = -1.0;
-	data->player.plane_x = -0.66;
-	data->player.plane_y = 0.0;
 }
