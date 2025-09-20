651057d7bae067a5db41271e1f90d0c08819fe42
halifecik <khalifecik@gmail.com>
2025-09-20 19:57:50 +0300

    fix: rename struct member

--- src/game/movement.c
+++ src/game/movement.c
@@ -6,7 +6,7 @@
 /*   By: hademirc <hademirc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/20 18:30:36 by hademirc          #+#    #+#             */
-/*   Updated: 2025/09/20 18:30:37 by hademirc         ###   ########.fr       */
+/*   Updated: 2025/09/20 19:53:42 by hademirc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void	ft_move_forward(t_data *data)
 	double	new_x;
 	double	new_y;
 
-	new_x = data->player.pos_x + data->player.dir_x * MOVE_SPEED;
-	new_y = data->player.pos_y + data->player.dir_y * MOVE_SPEED;
+	new_x = data->player.pos_x + data->player.dir_x * data->player.move_speed;
+	new_y = data->player.pos_y + data->player.dir_y * data->player.move_speed;
 	if (data->map.grid[(int)data->player.pos_y][(int)new_x] == '0')
 		data->player.pos_x = new_x;
 	if (data->map.grid[(int)new_y][(int)data->player.pos_x] == '0')
@@ -30,8 +30,8 @@ void	ft_move_backward(t_data *data)
 	double	new_x;
 	double	new_y;
 
-	new_x = data->player.pos_x - data->player.dir_x * MOVE_SPEED;
-	new_y = data->player.pos_y - data->player.dir_y * MOVE_SPEED;
+	new_x = data->player.pos_x - data->player.dir_x * data->player.move_speed;
+	new_y = data->player.pos_y - data->player.dir_y * data->player.move_speed;
 	if (data->map.grid[(int)data->player.pos_y][(int)new_x] == '0')
 		data->player.pos_x = new_x;
 	if (data->map.grid[(int)new_y][(int)data->player.pos_x] == '0')
@@ -43,8 +43,8 @@ void	ft_strafe_left(t_data *data)
 	double	new_x;
 	double	new_y;
 
-	new_x = data->player.pos_x - data->player.plane_x * MOVE_SPEED;
-	new_y = data->player.pos_y - data->player.plane_y * MOVE_SPEED;
+	new_x = data->player.pos_x - data->player.plane_x * data->player.move_speed;
+	new_y = data->player.pos_y - data->player.plane_y * data->player.move_speed;
 	if (data->map.grid[(int)data->player.pos_y][(int)new_x] == '0')
 		data->player.pos_x = new_x;
 	if (data->map.grid[(int)new_y][(int)data->player.pos_x] == '0')
@@ -56,8 +56,8 @@ void	ft_strafe_right(t_data *data)
 	double	new_x;
 	double	new_y;
 
-	new_x = data->player.pos_x + data->player.plane_x * MOVE_SPEED;
-	new_y = data->player.pos_y + data->player.plane_y * MOVE_SPEED;
+	new_x = data->player.pos_x + data->player.plane_x * data->player.move_speed;
+	new_y = data->player.pos_y + data->player.plane_y * data->player.move_speed;
 	if (data->map.grid[(int)data->player.pos_y][(int)new_x] == '0')
 		data->player.pos_x = new_x;
 	if (data->map.grid[(int)new_y][(int)data->player.pos_x] == '0')
