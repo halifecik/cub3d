8261c62347202725bec40a654d5b94f486afe12d
halifecik <khalifecik@gmail.com>
2025-09-22 21:06:38 +0300

    fix: norm issue

--- src/game/movement.c
+++ src/game/movement.c
@@ -6,7 +6,7 @@
 /*   By: hademirc <hademirc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/20 18:30:36 by hademirc          #+#    #+#             */
-/*   Updated: 2025/09/20 21:00:31 by hademirc         ###   ########.fr       */
+/*   Updated: 2025/09/22 21:05:16 by hademirc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,11 @@ void	ft_move_forward(t_data *data)
 
 	new_x = data->player.pos_x + data->player.dir_x * data->player.move_speed;
 	new_y = data->player.pos_y + data->player.dir_y * data->player.move_speed;
-	if (data->map.grid[(int)data->player.pos_y + data->map.map_index][(int)new_x] == '0')
+	if (data->map.grid[(int)data->player.pos_y
+			+ data->map.map_index][(int)new_x] == '0')
 		data->player.pos_x = new_x;
-	if (data->map.grid[(int)new_y + data->map.map_index][(int)data->player.pos_x] == '0')
+	if (data->map.grid[(int)new_y
+			+ data->map.map_index][(int)data->player.pos_x] == '0')
 		data->player.pos_y = new_y;
 }
 
@@ -32,9 +34,11 @@ void	ft_move_backward(t_data *data)
 
 	new_x = data->player.pos_x - data->player.dir_x * data->player.move_speed;
 	new_y = data->player.pos_y - data->player.dir_y * data->player.move_speed;
-	if (data->map.grid[(int)data->player.pos_y + data->map.map_index][(int)new_x] == '0')
+	if (data->map.grid[(int)data->player.pos_y
+			+ data->map.map_index][(int)new_x] == '0')
 		data->player.pos_x = new_x;
-	if (data->map.grid[(int)new_y + data->map.map_index][(int)data->player.pos_x] == '0')
+	if (data->map.grid[(int)new_y
+			+ data->map.map_index][(int)data->player.pos_x] == '0')
 		data->player.pos_y = new_y;
 }
 
@@ -45,9 +49,11 @@ void	ft_strafe_left(t_data *data)
 
 	new_x = data->player.pos_x - data->player.plane_x * data->player.move_speed;
 	new_y = data->player.pos_y - data->player.plane_y * data->player.move_speed;
-	if (data->map.grid[(int)data->player.pos_y + data->map.map_index][(int)new_x] == '0')
+	if (data->map.grid[(int)data->player.pos_y
+			+ data->map.map_index][(int)new_x] == '0')
 		data->player.pos_x = new_x;
-	if (data->map.grid[(int)new_y + data->map.map_index][(int)data->player.pos_x] == '0')
+	if (data->map.grid[(int)new_y
+			+ data->map.map_index][(int)data->player.pos_x] == '0')
 		data->player.pos_y = new_y;
 }
 
@@ -58,8 +64,10 @@ void	ft_strafe_right(t_data *data)
 
 	new_x = data->player.pos_x + data->player.plane_x * data->player.move_speed;
 	new_y = data->player.pos_y + data->player.plane_y * data->player.move_speed;
-	if (data->map.grid[(int)data->player.pos_y + data->map.map_index][(int)new_x] == '0')
+	if (data->map.grid[(int)data->player.pos_y
+			+ data->map.map_index][(int)new_x] == '0')
 		data->player.pos_x = new_x;
-	if (data->map.grid[(int)new_y + data->map.map_index][(int)data->player.pos_x] == '0')
+	if (data->map.grid[(int)new_y
+			+ data->map.map_index][(int)data->player.pos_x] == '0')
 		data->player.pos_y = new_y;
 }
