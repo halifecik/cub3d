5c6c4cd9ceb3d97aeb18b9373e28d17dfd496ee1
msgenan <msgenan07@gmail.com>
2025-09-25 17:54:27 +0300

    feat: add minimap frame

--- src/bonus/minimap.c
+++ src/bonus/minimap.c
@@ -2,17 +2,15 @@
 
 void	ft_initialize_minimap(t_minimap *minimap)
 {
-
-    minimap->half = 0;
-    minimap->start_x = 0;
-    minimap->start_y = 0;
-	minimap->cell_size = 30;
-	minimap->visible_cells = 10;
+	minimap->half = 0;
+	minimap->start_x = 0;
+	minimap->start_y = 0;
+	minimap->cell_size = 20;
+	minimap->visible_cells = 15;
 	minimap->width = minimap->cell_size * minimap->visible_cells;
 	minimap->height = minimap->width;
 }
 
-/* ------------------ Pixel ve Cell Fonksiyonları ------------------ */
 static void	ft_put_pixel(t_data *data, int x, int y, int color)
 {
 	char	*dst;
@@ -46,22 +44,49 @@ static void	ft_draw_cell(t_data *data, int cell_x, int cell_y, int color)
 	}
 }
 
-/* ------------------ Minimap Hücre Çizim Fonksiyonu ------------------ */
+static void	ft_draw_minimap_frame(t_data *data)
+{
+	int	i;
+	int	size;
+
+	size = data->minimap.visible_cells * data->minimap.cell_size;
+	i = 0;
+	while (i < size)
+	{
+		ft_put_pixel(data, i, 0, 0x000000);          // TOP
+		ft_put_pixel(data, i, size - 1, 0x000000);   // BOTTOM
+		i++;
+	}
+	i = 0;
+	while (i < size)
+	{
+		ft_put_pixel(data, 0, i, 0x000000);          // LEFT
+		ft_put_pixel(data, size - 1, i, 0x000000);   // RIGHT
+		i++;
+	}
+}
+
 static void	ft_draw_minimap_cell(t_data *data, int map_x, int map_y, int cell_x)
 {
 	char	tile;
 	int		color;
 
-	tile = data->map.grid[map_y][map_x];
-	if (tile == '1')
-		color = 0x444444; // Duvar gri
+	if (map_x >= 0 && map_x < data->map.width
+		&& map_y >= data->map.map_index && map_y < data->map.height)
+	{
+		tile = data->map.grid[map_y][map_x];
+		if (tile == '1')
+			color = 0x444444; // GRAY
+		else
+			color = 0xCCCCCC; // BRIGTH GRAY
+	}
 	else
-		color = 0xCCCCCC; // Boş alan açık gri
+		color = 0x000000; // BLACK
+
 	ft_draw_cell(data, cell_x % data->minimap.visible_cells,
 		cell_x / data->minimap.visible_cells, color);
 }
 
-/* ------------------ Minimap Çizim Fonksiyonu ------------------ */
 void	ft_draw_minimap(t_data *data)
 {
 	int	j;
@@ -80,11 +105,10 @@ void	ft_draw_minimap(t_data *data)
 		{
 			map_x = data->minimap.start_x + j;
 			map_y = data->minimap.start_y + i + data->map.map_index;
-			if (map_x >= 0 && map_x < data->map.width
-				&& map_y >= data->map.map_index && map_y < data->map.height)
-				ft_draw_minimap_cell(data, map_x, map_y,
-					i * data->minimap.visible_cells + j);
+			ft_draw_minimap_cell(data, map_x, map_y,
+				i * data->minimap.visible_cells + j);
 		}
 	}
 	ft_draw_cell(data, data->minimap.half, data->minimap.half, 0xFF0000);
+	ft_draw_minimap_frame(data);
 }
