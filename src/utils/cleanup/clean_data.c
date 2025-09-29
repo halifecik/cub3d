f08b032d73d1498a52ce6def6c3bca46120c71c6
halifecik <khalifecik@gmail.com>
2025-09-29 20:18:54 +0300

    fix: norm compliance

--- src/utils/cleanup/clean_data.c
+++ src/utils/cleanup/clean_data.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   clean_data.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hademirc <hademirc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/29 17:08:58 by hademirc          #+#    #+#             */
+/*   Updated: 2025/09/29 17:09:08 by hademirc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
 void	ft_clean_data(t_data *data)
@@ -10,9 +22,9 @@ void	ft_clean_data(t_data *data)
 	ft_clean_graphics(&data->graphics);
 }
 
-int    ft_close_window(t_data *data)
+int	ft_close_window(t_data *data)
 {
 	ft_clean_data(data);
 	exit(EXIT_SUCCESS);
-    return (0);
+	return (0);
 }
