db51d7b146e36cf12bee8676beebe1a04392ea4b
halifecik <khalifecik@gmail.com>
2025-09-29 17:22:12 +0300

    fix: norm compliance and restore big map functionallity

--- src/bonus/minimap/draw_full_map.c
+++ src/bonus/minimap/draw_full_map.c
@@ -1,24 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw_full_map.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hademirc <hademirc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/29 16:54:10 by hademirc          #+#    #+#             */
+/*   Updated: 2025/09/29 17:04:57 by hademirc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
 static int	fullmap_cell_color(t_data *data, int map_x, int map_y)
 {
-	int row_len;
+	int	row_len;
 
-	if (map_x < 0 || map_y < 0 || map_y >= data->map.height - data->map.map_index)
+	if (map_x < 0 || map_y < 0
+		|| map_y >= data->map.height - data->map.map_index)
 		return (BLACK_PIXEL);
 	row_len = ft_strlen(data->map.grid[map_y + data->map.map_index]);
 	if (map_x >= row_len)
 		return (BLACK_PIXEL);
-	return ft_tile_color(data->map.grid[map_y + data->map.map_index][map_x]);
+	return (ft_tile_color(data->map.grid[map_y + data->map.map_index][map_x]));
 }
 
-static void	draw_fullmap_cells(t_data *data, int cell_size, int offset_x, int offset_y)
+static void	draw_fullmap_cells(t_data *data, int cell_size, int offset_x,
+	int offset_y)
 {
-	int i;
-	int j;
-	int map_x;
-	int map_y;
-	int color;
+	int	i;
+	int	j;
+	int	map_x;
+	int	map_y;
+	int	color;
 
 	i = -1;
 	while (++i < data->map.height - data->map.map_index)
@@ -29,24 +43,26 @@ static void	draw_fullmap_cells(t_data *data, int cell_size, int offset_x, int of
 			map_x = j;
 			map_y = i;
 			color = fullmap_cell_color(data, map_x, map_y);
-			ft_draw_cell(data, offset_x + j * cell_size, offset_y + i * cell_size,
-				cell_size, color);
+			ft_draw_colored_cell(data, offset_x + j * cell_size,
+				offset_y + i * cell_size, color);
 		}
 	}
-	ft_draw_cell(data, offset_x + (int)data->player.pos_x * cell_size,
-		offset_y + (int)data->player.pos_y * cell_size, cell_size, RED_PIXEL);
+	ft_draw_colored_cell(data, offset_x + (int)data->player.pos_x * cell_size,
+		offset_y + (int)data->player.pos_y * cell_size, RED_PIXEL);
 }
 
 void	ft_draw_full_map(t_data *data)
 {
-	int cell_size;
-	int offset_x;
-	int offset_y;
+	int	cell_size;
+	int	offset_x;
+	int	offset_y;
 
 	cell_size = SCREEN_WIDTH / data->map.width;
 	if (cell_size > SCREEN_HEIGHT / (data->map.height - data->map.map_index))
 		cell_size = SCREEN_HEIGHT / (data->map.height - data->map.map_index);
+	data->minimap.current_cell_size = cell_size;
 	offset_x = (SCREEN_WIDTH - data->map.width * cell_size) / 2;
-	offset_y = (SCREEN_HEIGHT - (data->map.height - data->map.map_index) * cell_size) / 2;
+	offset_y = (SCREEN_HEIGHT - (data->map.height - data->map.map_index)
+			* cell_size) / 2;
 	draw_fullmap_cells(data, cell_size, offset_x, offset_y);
 }
--- src/bonus/minimap/draw_minimap.c
+++ src/bonus/minimap/draw_minimap.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw_minimap.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hademirc <hademirc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/29 16:54:22 by hademirc          #+#    #+#             */
+/*   Updated: 2025/09/29 17:05:01 by hademirc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
 void	ft_initialize_minimap(t_minimap *minimap)
@@ -9,27 +21,30 @@ void	ft_initialize_minimap(t_minimap *minimap)
 	minimap->visible_cells = 15;
 	minimap->width = minimap->cell_size * minimap->visible_cells;
 	minimap->height = minimap->width;
+	minimap->current_cell_size = minimap->cell_size;
 }
 
 static int	minimap_cell_color(t_data *data, int map_x, int map_y)
 {
-	int row_len;
+	int	row_len;
 
-	if (map_x < 0 || map_y < 0 || map_y >= data->map.height - data->map.map_index)
+	if (map_x < 0 || map_y < 0
+		|| map_y >= data->map.height - data->map.map_index)
 		return (BLACK_PIXEL);
 	row_len = ft_strlen(data->map.grid[map_y + data->map.map_index]);
 	if (map_x >= row_len)
 		return (BLACK_PIXEL);
-	return ft_tile_color(data->map.grid[map_y + data->map.map_index][map_x]);
+	return (ft_tile_color(data->map.grid[map_y + data->map.map_index][map_x]));
 }
 
-static void	draw_minimap_cells(t_data *data, int start_x, int start_y, int cell_size)
+static void	draw_minimap_cells(t_data *data, int start_x, int start_y,
+	int cell_size)
 {
-	int i;
-	int j;
-	int map_x;
-	int map_y;
-	int color;
+	int	i;
+	int	j;
+	int	map_x;
+	int	map_y;
+	int	color;
 
 	i = -1;
 	while (++i < data->minimap.visible_cells)
@@ -40,25 +55,25 @@ static void	draw_minimap_cells(t_data *data, int start_x, int start_y, int cell_
 			map_x = start_x + j;
 			map_y = start_y + i;
 			color = minimap_cell_color(data, map_x, map_y);
-			ft_draw_cell(data, j * cell_size, i * cell_size, cell_size, color);
+			ft_draw_colored_cell(data, j * cell_size, i * cell_size, color);
 		}
 	}
 }
 
 void	ft_draw_minimap(t_data *data)
 {
-	int cell_size;
-	int half;
-	int start_x;
-	int start_y;
+	int	cell_size;
+	int	half;
+	int	start_x;
+	int	start_y;
 
 	cell_size = data->minimap.cell_size;
+	data->minimap.current_cell_size = cell_size;
 	half = data->minimap.visible_cells / 2;
 	start_x = (int)data->player.pos_x - half;
 	start_y = (int)data->player.pos_y - half;
-
 	draw_minimap_cells(data, start_x, start_y, cell_size);
-	ft_draw_cell(data, half * cell_size, half * cell_size, cell_size, RED_PIXEL);
-	ft_draw_frame(data, 0, 0, data->minimap.visible_cells * cell_size,
-		data->minimap.visible_cells * cell_size);
+	ft_draw_colored_cell(data, half * cell_size, half * cell_size,
+		RED_PIXEL);
+	ft_draw_frame(data, 0, 0, data->minimap.visible_cells * cell_size);
 }
